7fc39d47a2cfedaf97c99b27a14d9e72ce068bae
MikeLitoris <maxime.genaudet@gmail.com>
2018-11-16 23:33:53 +0100

    bagdad

--- public/js/entities/enemies.js
+++ public/js/entities/enemies.js
@@ -8,13 +8,15 @@ game.MageEnemyEntity = me.Entity.extend({
     init: function(x, y, hp, points) {
 		
 		this._super(me.Entity, "init", [x, y, {width : 80, height : 80}]);
-		// create an animation using the cap guy sprites, and add as renderable
+
 		this.renderable = game.mageEnemyTexture.createAnimationFromName([
 				"2_ATTACK_001", "2_ATTACK_002", "2_ATTACK_003"
 		]);
-		this.renderable.addAnimation ("attack", [0,1,2]);
+		this.timerDelay = 1000;
+		this.renderable.addAnimation ("attack", [0,1,2],this.timerDelay/3);
+        this.startAttackTimer(this.timerDelay);
 		this.renderable.setCurrentAnimation("attack");
-		
+
         this.hp = hp;
         this.points = points;
         this.alwaysUpdate = true;
@@ -26,11 +28,11 @@ game.MageEnemyEntity = me.Entity.extend({
         this.body.vel.set(this.xGenere, this.yGenere);
     },
 	
-	onActivateEvent : function () {
+	startAttackTimer : function (timerDelay) {
 		var _this = this;
 		this.intervalID = me.timer.setInterval(function () {
-			me.game.world.addChild(new me.pool.pull('projectile', _this.pos.x - 5, _this.pos.y + 24), 14);
-		}, 1000);
+			me.game.world.addChild(new me.pool.pull('projectile', _this.pos.x - 5, _this.pos.y + 24, undefined, undefined, true, 8), 14);
+		}, timerDelay);
 	},	
 	
     update: function(dt) {
@@ -123,26 +125,32 @@ game.MeleeEnemyEntity = me.Entity.extend({
  */
 game.MageAttackEntity = me.Entity.extend({
 
-    init: function(x, y, velX, velY) {
+    init: function(x, y, velX, velY, explode, childs) {
 		var settings = {};
         settings.image = this.image = me.loader.getImage('projectile');
         settings.width = 22;
         settings.height= 22;
         settings.framewidth = 22;
         settings.frameheight = 22;
-
+        this.explosionDelay = 500;
         this._super(me.Entity, 'init', [x, y, settings]);
-
         this.alwaysUpdate = true;
         this.pos.add(this.body.vel);
-        this.body.gravity = 0;
+        this.body.gravity = new me.Vector2d(0,0);
         this.type = 'attack';
 		this.xGenere = velX;
 		this.yGenere = velY;
+		this.explode = explode;
+        this.childs = childs;
+		this.timeCreated = Date.now();
 		if(this.xGenere == undefined)
 			this.xGenere = -10;
 		if(this.yGenere == undefined)
 			this.yGenere = Number.prototype.random(-1, 1);
+        if(this.explode == undefined)
+            this.explode = false;
+        if(this.childs == undefined)
+            this.childs = 0;
         this.body.vel.set(this.xGenere, this.yGenere);
     },
 
@@ -154,6 +162,13 @@ game.MageAttackEntity = me.Entity.extend({
         }
         this.pos.add(this.body.vel);
         if (this.pos.x < -this.width || this.pos.y < -this.height || this.pos.y > me.game.viewport.width) {
+            me.timer.clearTimeout(this.timeoutID);
+            me.game.world.removeChild(this);
+        }
+        //explode
+        if(((Date.now() - this.timeCreated) >= this.explosionDelay) && this.explode){
+            this.createExplosion(this.childs);
+            this.explode = false;
             me.game.world.removeChild(this);
         }
 		this.body.vel.set(this.xGenere, this.yGenere);
@@ -164,5 +179,12 @@ game.MageAttackEntity = me.Entity.extend({
 	
 	destroy: function(){
 		
-	}
+	},
+
+    createExplosion : function (childs) {
+        var i;
+        for(i = 0; i < childs; i++) {
+            me.game.world.addChild(new me.pool.pull('projectile', this.pos.x, this.pos.y, Number.prototype.random(-5, 1), Number.prototype.random(-5, 5), false), 14);
+        }
+    }
 });
