25510e23580a915e044b01a8633c79834bc4373e
Maxime Genaudet <maxime.genaudet@soprabanking.com>
2018-11-26 13:18:31 +0100

    fix obfuscate

--- obfuscate.sh
+++ obfuscate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-javascript-obfuscator public/js/entities/entities.js --output  public/js/entities/entities.js
+javascript-obfuscator public/js/entities/boss.js --output  public/js/entities/boss.js
+javascript-obfuscator public/js/entities/enemies.js --output  public/js/entities/enemies.js
+javascript-obfuscator public/js/entities/generators.js --output  public/js/entities/generators.js
+javascript-obfuscator public/js/entities/pickups.js --output  public/js/entities/pickups.js
+javascript-obfuscator public/js/entities/santa.js --output  public/js/entities/santa.js
+javascript-obfuscator public/js/entities/weapons.js --output  public/js/entities/weapons.js
 javascript-obfuscator public/js/entities/HUD.js --output  public/js/entities/HUD.js
 javascript-obfuscator public/js/screens/gameover.js --output  public/js/screens/gameover.js
 javascript-obfuscator public/js/screens/play.js --output  public/js/screens/play.js
