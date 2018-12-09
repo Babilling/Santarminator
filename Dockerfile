eeb47d1738e5c5a07f44e89bb1d69f9c815ebd63
MikeLitoris <maxime.genaudet@gmail.com>
2018-12-09 17:17:34 +0100

    jème bi1 fér des dockers

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ COPY . /SpaceChristmas/
 
 ENV PORT=8080
 
+USER 0
+
 RUN chmod +x SpaceChristmas/entrypoint.sh
 
 ENTRYPOINT ["sh","SpaceChristmas/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i 's|8080|'"$PORT"'|g' SpaceChristmas/index.js
-cd SpaceChristmas && npm install && node index.js
+sed -i 's|8080|'"$PORT"'|g' spaceChristmas/index.js
+cd SpaceChristmas && npm install && npm link javascript-obfuscator && javascript-obfuscator ./public/js/ --output ./public/js/ && node index.js > spaceChristmas.log
