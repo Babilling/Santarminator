33bf802cf9621ed6e4fa887cb664ba078dbee138
MikeLitoris <maxime.genaudet@gmail.com>
2018-12-09 17:53:05 +0100

    Fix obfuscation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd SpaceChristmas && npm install && npm link javascript-obfuscator && javascript-obfuscator ./public/js/ --output ./public/js/ && node index.js > SpaceChristmas.log
+mv public/js/melonJS-min.js public/js/melonjs-min.js
+cd SpaceChristmas && npm install && npm link javascript-obfuscator && javascript-obfuscator public/js/ --output . && node index.js > SpaceChristmas.log
