70de54c1a83e386ad3855086dd542fee8baf13d7
MikeLitoris <maxime.genaudet@gmail.com>
2018-12-11 08:52:24 +0100

    externe db

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd santarminator && npm install && npm link javascript-obfuscator && javascript-obfuscator public/js/ --output . && node index.js > /etc/log/santarminator.log
+cd santarminator && npm install && npm link javascript-obfuscator && javascript-obfuscator public/js/ --output . && node index.js >> /etc/log/santarminator.log
