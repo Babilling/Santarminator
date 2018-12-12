11d0c4db569954fa7daf7dbacbcadc86093b47fd
Steven Pescheteau <steven.pescheteau@gmail.com>
2018-12-12 03:39:01 +0100

    difficulté lissée. Il est beaucoup plus simple d'atteindre un boss

--- public/js/entities/generators.js
+++ public/js/entities/generators.js
@@ -8,54 +8,64 @@ game.EnnemyGenerator = me.Renderable.extend({
     init: function() {
         this._super(me.Renderable, 'init', [0, me.game.viewport.width, me.game.viewport.height, 80]);
         this.alwaysUpdate = true;
-        this.generate = 0;
-        this.pipeFrequency = 80;
         this.posX = me.game.viewport.width;
         this.scorePopBoss = 500;
         this.bossPopped = 0;
         this.boss = false;
         this.difficulty = 1;
+        this.cdBeforeSpawn = 2000;
+        this.lastSpawn = 0;
     },
 
     update: function(dt) {
-        if (this.generate++ % this.pipeFrequency == 0) {
-            let posX = me.game.viewport.width + 50;
-            let posY = me.Math.random(100, me.video.renderer.getHeight() - 100);
-            let enemiesHp = this.difficulty * 5;
-            if (enemiesHp > 20) enemiesHp = 20;
-            let enemiesPoints = 10;
-			if(!this.boss && game.data.steps > (this.bossPopped + 1) * this.scorePopBoss + (this.bossPopped * 1000)) {
-                switch(me.Math.random(1, 4)) {
-                    case 1 :
-                        game.boss = new me.pool.pull('mageBoss', me.game.viewport.width + 50, 50, undefined, undefined, enemiesHp * 50);
-						me.audio.play("intro_pere_fougasse");
-                        break;
-                    case 2 :
-                        game.boss = new me.pool.pull('treeBoss', me.game.viewport.width + 50, 50, undefined, undefined, enemiesHp * 50);
-                        break;
-                    case 3 :
-                        game.boss = new me.pool.pull('unicornBoss', me.game.viewport.width + 50, 50, undefined, undefined, enemiesHp * 50);
-                        break;
-                }
-			    
-                me.game.world.addChild(game.boss, 12);
-                this.boss = true;
-                this.bossPopped++;
-                
-            }else if (!this.boss)
-                switch(me.Math.random(1, 4)) {
-                    case 1 :
-                        this.chainPattern(me.Math.random(this.difficulty + 1, this.difficulty + 3), posX, posY, undefined, me.Math.degToRad(me.Math.random(-80, 81)), enemiesHp, enemiesPoints);
-                        break;
-                    case 2 :
-                        this.linePattern(me.Math.random(this.difficulty, this.difficulty + 2), posX, posY, -2, me.Math.degToRad(me.Math.random(-80, 81)), enemiesHp, enemiesPoints, me.Math.random() >= 0.5);
-                        break;
-                    case 3 :
-                        this.linePattern(me.Math.random(this.difficulty, this.difficulty + 2), posX, posY, -2, me.Math.degToRad(me.Math.random(-80, 81)), enemiesHp, enemiesPoints, me.Math.random() >= 0.5);
-                        break;
-                }
+        if (Date.now() - this.lastSpawn > this.cdBeforeSpawn){
+            this.lastSpawn = Date.now();
+            var gap = Math.round((2 + Math.log(game.data.steps + 1) / 4) * ((this.bossPopped + 1) / 2)); 
+            var enemyNumber = 0;
+            me.game.world.forEach(function (child) {
+                if (child.type === 'ennemy')
+                    enemyNumber++;
+            });
+            console.log(gap);
+            if (enemyNumber < gap) {
+                let posX = me.game.viewport.width + 50;
+                let posY = me.Math.random(100, me.video.renderer.getHeight() - 100);
+                let enemiesHp = this.difficulty * 5;
+                if (enemiesHp > 20) enemiesHp = 20;
+                let enemiesPoints = 10;
+                if(!this.boss && game.data.steps > (this.bossPopped + 1) * this.scorePopBoss + (this.bossPopped * 1000)) {
+                    switch(me.Math.random(1, 4)) {
+                        case 1 :
+                            game.boss = new me.pool.pull('mageBoss', me.game.viewport.width + 50, 50, undefined, undefined, enemiesHp * 50);
+                            me.audio.play("intro_pere_fougasse");
+                            break;
+                        case 2 :
+                            game.boss = new me.pool.pull('treeBoss', me.game.viewport.width + 50, 50, undefined, undefined, enemiesHp * 50);
+                            break;
+                        case 3 :
+                            game.boss = new me.pool.pull('unicornBoss', me.game.viewport.width + 50, 50, undefined, undefined, enemiesHp * 50);
+                            break;
+                    }
+                    
+                    me.game.world.addChild(game.boss, 12);
+                    this.boss = true;
+                    this.bossPopped++;
+                    
+                }else if (!this.boss)
+                    switch(me.Math.random(1, 4)) {
+                        case 1 :
+                            this.chainPattern(me.Math.random(this.difficulty + 1, this.difficulty + 3), posX, posY, undefined, me.Math.degToRad(me.Math.random(-80, 81)), enemiesHp, enemiesPoints);
+                            break;
+                        case 2 :
+                            this.linePattern(me.Math.random(this.difficulty, this.difficulty + 2), posX, posY, -2, me.Math.degToRad(me.Math.random(-80, 81)), enemiesHp, enemiesPoints, me.Math.random() >= 0.5);
+                            break;
+                        case 3 :
+                            this.linePattern(me.Math.random(this.difficulty, this.difficulty + 2), posX, posY, -2, me.Math.degToRad(me.Math.random(-80, 81)), enemiesHp, enemiesPoints, me.Math.random() >= 0.5);
+                            break;
+                    }
+            }
+            this._super(me.Entity, "update", [dt]);
         }
-        this._super(me.Entity, "update", [dt]);
     },
     chainPattern: function(size, posX, posY, speed, radX, enemiesHp, enemiesPoints){
         if (posX === undefined) posX = me.game.viewport.width + 50;
@@ -83,5 +93,3 @@ game.EnnemyGenerator = me.Renderable.extend({
         }
     },
 });
-
-
